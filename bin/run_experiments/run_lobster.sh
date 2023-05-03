f230f1d206fa2036babf078bbaf00bb7111a3065
Peer Nagy <peer.nagy@icloud.com>
2023-05-03 19:07:03 +0000

    arc script

--- bin/run_experiments/run_lobster.sh
+++ bin/run_experiments/run_lobster.sh
@@ -1,4 +1,4 @@
-python run_train.py --C_init=trunc_standard_normal --prenorm=True --batchnorm=True --bidirectional=True \
+python run_train.py --C_init=trunc_standard_normal --prenorm=True --batchnorm=True --bidirectional=False \
                     --blocks=8 --bsz=16 --d_model=32 --dataset=lobster-prediction \
                     --dir_name='./data' --clip_eigs=True \
                     --dt_global=False --epochs=100 --jax_seed=42 --lr_factor=1 --n_layers=6 \
