d61ddafcb8d9b50ee6fb21ce5a519db4f5b8d8a5
Peer Nagy <peer.nagy@icloud.com>
2023-05-12 15:48:07 +0000

    cleanup

--- bin/run_experiments/run_lobster.sh
+++ bin/run_experiments/run_lobster.sh
@@ -4,4 +4,4 @@ python run_train.py --C_init=trunc_standard_normal --prenorm=True --batchnorm=Tr
                     --dt_global=False --epochs=100 --jax_seed=42 --lr_factor=1 --n_layers=6 \
                     --opt_config=BandCdecay --p_dropout=0.2 --ssm_lr_base=0.0005 --ssm_size_base=256 \
                     --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
-                    --use_book_data=True --masking=causal --num_devices=2
+                    --use_book_data=True --masking=causal --num_devices=1
--- run_train.py
+++ run_train.py
@@ -1,5 +1,6 @@
-import os
-os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=48'
+# CAVE: only for debugging purposes
+#import os
+#os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=48'
 
 import argparse
 from s5.utils.util import str2bool
