bd6439d195a385e6b106f8d60942a5892425e51d
Peer Nagy <peer.nagy@icloud.com>
2023-05-27 13:43:26 +0000

    run_arc

--- bin/run_experiments/run_lobster_arc.sh
+++ bin/run_experiments/run_lobster_arc.sh
@@ -1,9 +1,9 @@
 python run_train.py --C_init=trunc_standard_normal --prenorm=True --batchnorm=True --bidirectional=True \
-                    --blocks=8 --bsz=8 --d_model=128 --dataset=lobster-prediction \
-                    --dir_name='./data/test' --clip_eigs=True \
+                    --blocks=8 --bsz=16 --d_model=128 --dataset=lobster-prediction \
+                    --dir_name='./data' --clip_eigs=True \
                     --dt_global=False --epochs=100 --jax_seed=42 --lr_factor=1 --n_layers=6 \
                     --opt_config=BandCdecay --p_dropout=0.2 --ssm_lr_base=0.0005 --ssm_size_base=256 \
                     --warmup_end=1 --weight_decay=0.05 --msg_seq_len=500 \
                     --use_book_data=True --use_simple_book=False --book_transform=True  \
                     --masking=causal \
-                    --num_devices=2 --n_data_workers=0
+                    --n_data_workers=0
