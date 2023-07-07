055f334f1e5cc849fb44eb37cd2f7835d720c5a6
Peer Nagy <peer.nagy@icloud.com>
2023-07-07 08:34:48 +0000

    train script

--- bin/run_experiments/run_lobster_arc.sh
+++ bin/run_experiments/run_lobster_arc.sh
@@ -1,5 +1,5 @@
 python run_train.py --C_init=trunc_standard_normal --prenorm=True --batchnorm=True --bidirectional=True \
-                    --blocks=8 --bsz=40 --d_model=128 --dataset=lobster-prediction \
+                    --blocks=8 --bsz=32 --d_model=128 --dataset=lobster-prediction \
                     --dir_name='./data/fast_encoding' --clip_eigs=True --activation_fn=half_glu1 \
                     --dt_global=False --epochs=100 --jax_seed=42 --lr_factor=1 --n_layers=6 \
                     --opt_config=standard --p_dropout=0.0 --ssm_lr_base=0.0005 --ssm_size_base=256 \
